6e9af5c0926fa4dfb98c49af1c7ccaf242dab980
Restu Adi Wahyujati <adijati1029@gmail.com>
2024-12-24 11:42:14 +0700

    backend

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(
     //     allowedHeaders: ['Content-Type', 'Authorization'],
     // }
 );
-app.options('*', cors());
+// app.options('*', cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
