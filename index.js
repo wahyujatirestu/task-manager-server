edab2fe45d7adeb9efef89706f0a8e3353a4f587
Restu Adi Wahyujati <adijati1029@gmail.com>
2024-12-24 11:19:25 +0700

    backend

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.use(
         origin: process.env.ORIGIN,
         credentials: true,
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
     })
 );
 app.options('*', cors());
