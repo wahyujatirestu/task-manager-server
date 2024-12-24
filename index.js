f2b22c2ffb8614267a10475157a0b448a9591e29
Restu Adi Wahyujati <adijati1029@gmail.com>
2024-12-24 13:04:01 +0700

    backend

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: process.env.ORIGIN,
+        origin: 'https://jastrate-task-manager.vercel.app/',
         credentials: true,
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     })
