4066dc721641684364b6de8c7071316294508fb9
Restu Adi Wahyujati <adijati1029@gmail.com>
2025-09-17 23:47:21 +0700

    fix: update docker-compose command and scripts

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: process.env.ORIGIN,
+        origin: [process.env.ORIGIN, process.env.ORIGIN1],
         credentials: true,
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     })
